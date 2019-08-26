de75b9791dea9cbb12a28da7f4f375d2f4daff3d
Josh Wu <josh.wu@tradevan.com.tw>
2019-08-26 09:47:18 +0800

    Updates

---
+++ precache-manifest.ec5a988559a083e01b23f5ce082c2a8b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6c8c9ac9459a1bd49cf5d78174f14d4",
+    "url": "/Week6_hotel_reservation/index.html"
+  },
+  {
+    "revision": "5ee2c93b4426b7988e62",
+    "url": "/Week6_hotel_reservation/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "242bd5f109b9d9a28e18",
+    "url": "/Week6_hotel_reservation/static/js/2.2f529f07.chunk.js"
+  },
+  {
+    "revision": "5ee2c93b4426b7988e62",
+    "url": "/Week6_hotel_reservation/static/js/main.fe99f17a.chunk.js"
+  },
+  {
+    "revision": "68ab111ae41475fdd6df",
+    "url": "/Week6_hotel_reservation/static/js/runtime~main.c65cd6a4.js"
+  },
+  {
+    "revision": "a1b1533cc5299b0bfc0fbf8ead74352a",
+    "url": "/Week6_hotel_reservation/static/media/envelope-solid.a1b1533c.svg"
+  },
+  {
+    "revision": "7df7570f7e5bb9b2f7dc29aded8c7238",
+    "url": "/Week6_hotel_reservation/static/media/facebook-square-brands.7df7570f.svg"
+  },
+  {
+    "revision": "1600889cd9dea93d957dd508ab0a4976",
+    "url": "/Week6_hotel_reservation/static/media/home-solid.1600889c.svg"
+  },
+  {
+    "revision": "cc5e92b0767b99437ee32d191c9c2342",
+    "url": "/Week6_hotel_reservation/static/media/instagram-brands.cc5e92b0.svg"
+  },
+  {
+    "revision": "6dbf65e5c980bded8b6b6ca3b025af3f",
+    "url": "/Week6_hotel_reservation/static/media/phone-alt-solid.6dbf65e5.svg"
+  }
+]);
